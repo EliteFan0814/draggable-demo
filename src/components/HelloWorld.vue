321fb4073398a9130592f237e6f7ac7cb114dada
fanpeichao <fan552426811@outlook.com>
2021-12-27 17:31:46 +0800

    add all

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,130 +1,174 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <!-- 左侧组件列表 -->
+    <aside>
+      <ul>
+        <li v-for="item in compList" :key="item.id" draggable @dragstart="handleDragStart" @dragend="handleDragEnd"
+          :data-comp-type="item.name" :data-text="item.text">
+          {{item.text}}
+        </li>
+      </ul>
+    </aside>
+    <!-- 中间组件预览排列 -->
+    <main class="draggable-wrap" @dragover="handleDragOver" @drop="handleDrop">
+      <draggable v-model="compShowList" draggable=".draggable-item" :sort="true" chosen-class="chosen"
+        :forceFallback="true" animation="600">
+        <div class="draggable-item" v-for="(element,index) in compShowList" :data-index="index" :key="index">
+          <div v-if="element.isMoving" class="dragging-content">移动中</div>
+          <div v-else class="item-content"> {{element.text}}</div>
+        </div>
+      </draggable>
+    </main>
   </div>
 </template>
 
 <script>
+import draggable from 'vuedraggable'
+import { mapGetters, mapMutations } from 'vuex'
+
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  name: 'HelloWorld',
+  components: {
+    draggable
   },
-};
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      compList: [
+        { name: 'banner', text: '轮播图' },
+        { name: 'goods', text: '商品' },
+        { name: 'spread', text: '通告' }
+      ],
+      compShowList: [],
+      // 是否正在拖动某元素
+      isDragging: false,
+      // 正在拖动的元素组件类型
+      draggingCompType: null,
+      draggingCompText: null,
+      // 当前添加的元素在compShowList的角标
+      currentaddCompIndex: 0
+    }
+  },
+  // computed: {
+  //   compShowList: {
+  //     get() {
+  //       return this.$store.state.compShowList
+  //     },
+  //     set(value) {
+  //       this.$store.commit('updateList', value)
+  //     }
+  //   }
+  // },
+  watch: {
+    myArray(newvalue, oldvalue) {
+      // console.log(newvalue)
+    },
+    compShowList() {
+      // console.log(this.$store.getters.compShowList)
+    }
+  },
+  methods: {
+    // 拖动开始
+    handleDragStart(e) {
+      this.draggingCompType = e.target.dataset.compType
+      this.draggingCompText = e.target.dataset.text
+      // e.dataTransfer.setData('comp', e.target.dataset.compType)
+    },
+    // 拖动结束
+    handleDragEnd() {
+      this.compShowList[this.currentaddCompIndex].isMoving = false
+      this.isDragging = false
+      this.draggingCompType = null
+    },
+    // 在某元素上方拖动
+    handleDragOver(e) {
+      e.preventDefault()
+      e.stopPropagation()
+      // 获取在哪个元素上方拖动
+      const targetDiv = e.target.className
+      // 如果在包裹器内浮动
+      if (targetDiv === 'draggable-wrap') {
+        const addingComp = { name: this.draggingCompType, isMoving: true, text: this.draggingCompText }
+        if (!this.isDragging) {
+          this.currentaddCompIndex = this.compShowList.length
+          this.isDragging = true
+          this.compShowList.push(addingComp)
+        }
+        // 如果在包裹器内添加的元素上浮动
+      } else if (targetDiv === 'draggable-item') {
+        const addingComp = { name: this.draggingCompType, isMoving: true, text: this.draggingCompText }
+        // 获取浮动在内部元素的位置信息 距离顶部距离、目标元素高度、目标元素排序
+        let [y, overCompHeight, overCompIndex] = [e.offsetY, e.target.offsetHeight, e.target.dataset.index]
+        //  若为 true 放到目标元素上方，否则为下方
+        let direction = y < overCompHeight / 2
+        if (!this.isDragging) {
+          if (direction) {
+            if (overCompIndex === 0) {
+              this.compShowList.unshift(addingComp)
+            }
+          }
+        }
+        if (direction) {
+          console.log('overCompIndex', overCompIndex)
+          if (!this.compShowList[overCompIndex - 1].isMoving) {
+            // 在元素上方插入
+            const temp = this.compShowList.splice(this.currentaddCompIndex, 1)
+            this.compShowList.splice(overCompIndex, 0, temp[0])
+          }
+        } else {
+          console.log(333)
+          console.log('overCompIndex', overCompIndex)
+          console.log('currentaddCompIndex', this.currentaddCompIndex)
+          console.log('compShowListLength', this.compShowList.length)
+          console.log('compShowList', this.compShowList)
+          // 在元素下方插入
+          const temp = this.compShowList.splice(this.currentaddCompIndex, 1)
+          this.compShowList.splice(overCompIndex + 1, 0, temp[0])
+        }
+      }
+    },
+    // 触发在 draggable-wrap 上的放置事件
+    handleDrop(e) {
+      e.preventDefault()
+      e.stopPropagation()
+      this.isDragging = false
+    }
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hello {
+  display: flex;
+  height: 100vh;
+  aside {
+    border: 1px solid red;
+    width: 180px;
+    ul {
+      padding: 0;
+      margin: 0;
+      list-style: none;
+      li {
+        padding: 20px;
+        border: 1px solid;
+        margin: 0;
+        list-style: none;
+      }
+    }
+  }
+  main {
+    border: 1px solid green;
+    width: 50%;
+    .draggable-item {
+      padding: 50px 0;
+      border: 1px solid rgba(166, 255, 0, 0.959);
+    }
+    .chosen {
+      border: solid 2px #3089dc !important;
+    }
+  }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,25 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    myList: [
+      { id: 1, name: 'fpdddc' },
+      { id: 2, name: 'syq' },
+      { id: 3, name: 'fmg' },
+      { id: 4, name: 'eee' }
+    ]
+  },
+  getters: {
+    myList: (state) => state.myList
+  },
+  mutations: {
+    updateList(state, value) {
+      state.myList = value
+    }
+  },
   actions: {},
-  modules: {},
-});
+  modules: {}
+})
